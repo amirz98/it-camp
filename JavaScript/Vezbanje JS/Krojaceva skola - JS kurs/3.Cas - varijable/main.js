5fe6e925df09955826951761d698c936ab652318
amirz98 <amirzornic1@gmail.com>
2022-10-19 23:02:37 +0200

    commitments

---
+++ JavaScript/Vezbanje JS/Krojaceva skola - JS kurs/3.Cas - varijable/main.js
@@ -0,0 +1,20 @@
+var broj = 12;
+var broj1 = 22;
+var BROJ = 33;
+
+// alert(broj);
+// alert(broj);
+
+console.log(broj);
+
+// VARIJABLA NE SME POCINJATI SA BROJEM //
+// MOZE POCETI DONJOM CRTOM _ ALI SAMO KADA CEMO NAGLASITI DA JE NESTO JAKO BITNO
+// MOZE SE POCETI I ZNAKOM $
+// MOZE POCETI VELIKIM SLOVOM
+// PRAKSA JE DA SE PISE MALIM SLOVIMA
+// MOZE SE PISATI SVASTA, SEM REZERVISANIH RECI KOJE KORISTIMO U JAVASCRIPT
+
+var broj = 12;
+broj = 100;
+//menjamo vrednost varijable, na dalje svako koriscenje varijable "broj" bice 100
+console.log(broj);
