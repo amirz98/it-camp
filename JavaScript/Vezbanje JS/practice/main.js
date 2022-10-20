cb4e22a03f86993cf305367595d98e0fa3b644d5
amirz98 <amirzornic1@gmail.com>
2022-10-20 22:01:15 +0200

    kuckanje

---
+++ JavaScript/Vezbanje JS/practice/main.js
@@ -0,0 +1,36 @@
+var broj = 10;
+console.log(broj);
+
+var broj2 = 5;
+console.log(broj % broj2);
+
+broj = 11;
+
+console.log(broj % broj2);
+
+alert(broj2 + broj);
+
+var tekst = "Hello\t World!";
+// console.log(tekst);
+
+var tekst2 = "\nUcim JAVASCRIPT";
+// console.log(tekst2);
+
+console.log(tekst + tekst2);
+
+console.log(broj + tekst);
+
+var ime = "Amir\n";
+var godine = "24\n";
+var zanimanje = "developer";
+
+console.log(ime + godine + zanimanje);
+
+var car;
+console.log(car);
+
+var motocycle = null;
+console.log(motocycle);
+
+var text = "komentar u konzoli";
+console.log(text);
