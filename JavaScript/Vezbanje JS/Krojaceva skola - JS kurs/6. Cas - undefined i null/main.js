81b08c64885049588909b1dddaccdac39639455e
amirz98 <amirzornic1@gmail.com>
2022-10-20 21:40:45 +0200

    undefined i null - 6 cas

---
+++ JavaScript/Vezbanje JS/Krojaceva skola - JS kurs/6. Cas - undefined i null/main.js
@@ -0,0 +1,16 @@
+// undefined i null predstavljaju nedostatak vrednosti
+
+// var car = "volvo";
+// console.log(car);
+
+// var car;
+// console.log(car);
+// undefined - vrednost koju je pisao sam JS posto je video da je kockica prazna
+// kada god vidimo undefined u kozoli to posmatramo kao gresku
+// jer smo dozvolili javascriptu da pise umesto nas neku vrednost
+
+// var car = null;
+// console.log(car);
+// razlika izmedju undefined i null jeste sto ja znam da sam licno uticao da varijabla bude prazna (null),
+// u slucaju da zelim kasnije da dodelim vrednost
+// dok u slucaju undefined JS se pobrinuo da popuni neko prazno mesto koje ja nisam popunio - i to je greska
