1dcbd6785f0731a2969ccf1d45ef202c51cf6ce4
amirz98 <amirzornic1@gmail.com>
2022-10-20 21:10:44 +0200

    numbers

---
+++ JavaScript/Vezbanje JS/Krojaceva skola - JS kurs/5. Cas - numbers/main.js
@@ -0,0 +1,16 @@
+var num = 6;
+var num2 = 13;
+console.log(num2 % num);
+
+// U slucaju da je data type broj on (+) ce izvrsiti matematicku radnju
+// U slucaju da je data type string on (+) ce izvrsiti samo spajanje
+
+// remainder (ostatak) - % predstavlja koliko puta se neki broj (num) sadrzi u tom broju (num2), ostatak koji ostane je rezultat
+// broj 6 se nalazi u broju 13 dva puta, ostaje 1, dakle 1%.
+
+console.log("Danilo" + num);
+// sve sabrano sa stringom uvek se drugi data type pretvara u string, a onda se izvrisiti konkatinacija tj spajanje.
+
+var num = "4";
+var num2 = "9";
+console.log(num + num2);
