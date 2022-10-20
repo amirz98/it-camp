cdc50a196e9c5f6ecd7e60d94bd0e85777019182
amirz98 <amirzornic1@gmail.com>
2022-10-20 19:15:57 +0200

    tipovi podataka (string)

---
+++ JavaScript/Vezbanje JS/Krojaceva skola - JS kurs/4. Cas - tipovi podataka (string)/main.js
@@ -0,0 +1,35 @@
+// Tipove podataka u JavaScriptu delimo na proste i kompleksne
+// U proste tipove spadaju:
+// 1. string (obican tekst)
+// 2. numbers (brojevi)
+// 3. undefined
+// 4. null
+// 5. boolean
+
+// U kompleksne tipove spadaju:
+// 1. array (lista)
+// 2. object (objekat)
+// 3. function (funkcija)
+
+// Razlika je u tome sto prosti sadrze uvek samo jednu vrednost,
+// dok kompleksni kao sto im i ime kaze sadrze vise vrednosti u sebi
+
+// 1. STRING
+// string je obican tekst koji se nadje u nasem kodu
+
+// var tekst = "Ucim Javascript";
+// var tekst2 = "Ucim JS";
+// console.log(tekst2);
+
+// var name = "Danijel";
+// var job = "developer";
+// var nj = name + job;
+// console.log(nj);
+// console.log(name + " " + job);
+
+var tekst = 'Danas \nje "divan" \t \tdan';
+console.log(tekst);
+
+// Kontra kosa crta (\) - sledeci karakter posle kontra kose crte JavaScript ne tumaci kao sastavni deo JavaScript-a
+// \n - novi red u konzoli (kao new)
+// \t razmak (kao tab)
