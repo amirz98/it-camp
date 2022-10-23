5692dee9b2082a9bc707265451d1ab9e331134c1
amirz98 <amirzornic1@gmail.com>
2022-10-23 12:42:19 +0200

    vezb

---
+++ JavaScript/Vezbanje JS/Ternarni operator if/app.js
@@ -0,0 +1,14 @@
+// let isSunny = true;
+let isSunny = false;
+
+isSunny
+  ? console.log("Vreme je suncano, ponesite naocare")
+  : console.log("Vreme je kisovito, ponesite kisobran");
+
+let prom1 = 5;
+let prom2 = 10;
+let prom3 = 11;
+
+prom1 > 0 && prom2 > 0 && prom3 > 0
+  ? console.log("sve tri promenljive su vece od nule")
+  : console.log("sve tri promenljive su manje od nule");
