a78d249069ad9c09405106954f1897ab431bfa64
amirz98 <amirzornic1@gmail.com>
2022-10-26 01:14:41 +0200

    if else switch case prompt

---
+++ JavaScript/Vezbanje JS/vezba/app.js
@@ -0,0 +1,119 @@
+// const ime = prompt("Unesite vase ime");
+// console.log(ime);
+
+// const godine = prompt("Unesiste vase godine");
+// console.log(typeof godine);
+
+// const brGodina = Number(godine);
+// console.log(brGodina);
+// console.log(typeof brGodina);
+
+// const brGodina2 = +godine;
+// console.log(brGodina2);
+// console.log(typeof brGodina2);
+
+// const visina = +prompt("unesite svoju visinu u cm:");
+// console.log(visina);
+// console.log(typeof visina);
+
+// var name = prompt("what is your name?");
+// alert("hello " + name);
+
+// let hour = 22;
+
+// if (hour >= 6 && hour < 12) {
+//   console.log("Good morning!");
+// } else if (hour >= 12 && hour < 18) {
+//   console.log("Good afternoon!");
+// } else console.log("Good evening!");
+
+// Ako je broj godina unet od strane korisnika >= 18,
+// neka se ispise poruka "punoletni ste",
+// u suprotnom "maloletni ste".
+
+// const godine = prompt("Unesite broj godina?!");
+// if (godine >= 18) {
+//   console.log("Punoletni ste!");
+// } else if (godine <= 18 && godine > 0) {
+//   console.log("Maloletni ste!");
+// } else console.log("Niste uneli broj godina!");
+
+// Na osnovu unetih godina ispisati u konzoli sledece:
+// ako je broj godina manji od 12: Vi ste decijeg doba
+// ako je broj godina izmedju 12(ukljucuje) i 18(ne ukljucuje): Vi ste maloletni
+// ako je broj godina izmedju 18(ukljucuje) i 40(ne ukljucuje): Vi ste punoletni
+// ako je broj godina veci ili jednak 40: Vi ste zrela osoba
+// ako korisnik nije uneo broj: Niste uneli broj godina.
+
+// const godine = prompt("Unesite broj godina");
+// if (godine < 12) {
+//   console.log("Vi ste decijeg doba");
+// } else if (godine >= 12 && godine < 18) {
+//   console.log("Vi ste maloletni!");
+// } else if (godine >= 18 && godine < 40) {
+//   console.log("Vi ste punoletni!");
+// } else if (godine >= 40) {
+//   console.log("Vi ste zrela osoba");
+// } else console.log("Niste uneli broj");
+
+// ukljucuje se i 12 i 15
+// const broj = +prompt("Unesite broj izmedju 12 i 15: ");
+// switch (broj) {
+//   case 12:
+//     console.log("Unet je broj 12.");
+//     break;
+//   case 13:
+//     console.log("Unet je broj 13.");
+//     break;
+//   case 14:
+//     console.log("Unet je broj 14.");
+//     break;
+//   case 15:
+//     console.log("Unet je broj 15.");
+//     break;
+//   default:
+//     console.log("Nije unet trazeni broj.");
+// }
+
+// let text;
+// let favDrink = prompt("What is your favorite drink?");
+// switch (favDrink) {
+//   case "Cockta":
+//     console.log("Nice chois");
+//     break;
+//   case "Fanta":
+//     console.log("Really? Me too!");
+//     break;
+//   case "Sprite":
+//     console.log("Are you sure?");
+//     break;
+//   default:
+//     console.log("I have never heard of that one before!");
+// }
+
+// let text;
+// let date = +prompt("Which day is today?");
+// switch (date) {
+//   case 0:
+//     console.log("Monday");
+//     break;
+//   case 1:
+//     console.log("Tuesday");
+//     break;
+//   case 2:
+//     console.log("Wednesday");
+//     break;
+//   case 3:
+//     console.log("Thursday");
+//     break;
+//   case 4:
+//     console.log("Friday");
+//     break;
+//   case 5:
+//     console.log("Saturday");
+//     break;
+//   case 6:
+//     console.log("Sunday");
+//   default:
+//     console.log("Non-existent");
+// }
