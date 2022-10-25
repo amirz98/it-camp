4e730875c48853b7f978fd6c642e504fe83b9fd2
amirz98 <amirzornic1@gmail.com>
2022-10-25 16:11:42 +0200

    array

---
+++ JavaScript/Vezbanje JS/Krojaceva skola - JS kurs/7. Cas - Array (liste)/main.js
@@ -0,0 +1,12 @@
+// array je indexno uredjeni skup elemenata
+// indexi nam omogucavaju da dodjemo do posebnog elementa iz nase liste
+// broj elemenata u listi je neogranicen
+// unutar array-a svaki od elemenata moze biti svaki validan datatype - DEFINICIJA
+
+var cars = ["Skoda", "Volvo", "Audi", 22, undefined, null, [1, 2, 3]];
+// console.log(cars);
+// console.log(cars[0]);
+// console.log(cars[3]);
+console.log(cars[6][1]);
+
+// Liste su osnovni datatype kako cuvamo visestruke elemente
