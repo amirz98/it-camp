25afa69706c35d237df1c4487fd8ed755ae86b0d
amirz98 <amirzornic1@gmail.com>
2022-10-27 21:00:02 +0200

    for petlja

---
+++ JavaScript/Casovi/6. Cas/domaci.js
@@ -0,0 +1,28 @@
+// const prviBroj = Number(prompt("Unesite prvi broj:"));
+// const drugiBroj = +prompt("Unesite drugi broj");
+
+// if (isNaN(prviBroj) || isNaN(drugiBroj)) {
+//   console.log("Unete vrednosti moraju biti brojevi");
+// } else if (prviBroj === drugiBroj) {
+//   console.log(
+//     "Povrsina kvadrata na osnovu unetih stranica iznosi" + prviBroj * drugiBroj
+//   );
+// } else if (prviBroj !== drugiBroj) {
+//   console.log(
+//     "Povrsina pravougaonika na osnovu unetih stranica iznosi" +
+//       prviBroj * drugiBroj
+//   );
+// }
+
+// Za proveru da li je vrednost nekog broja NaN se koristi metoda
+// isNaN(vrednost)
+
+// const x = Number(prompt("Uunesite prvi broj"));
+// const y = +prompt("Unesite drugi broj");
+// if (isNaN(x) || isNaN(y)) {
+//   console.log("Vrednosti moraju biti brojevi");
+// } else if (y === 0) {
+//   console.log("Deljenje nulom nije moguce");
+// } else {
+//   console.log("Kolicnik unetih brojeva je: " + x / y);
+// }
---
+++ JavaScript/Casovi/6. Cas/main.js
@@ -0,0 +1,73 @@
+// Ispisati prvih deset brojeva u konzoli , jedan ispod drugog.
+console.log(1);
+console.log(2);
+console.log(3);
+console.log(4);
+console.log(5);
+console.log(6);
+console.log(7);
+console.log(8);
+console.log(9);
+console.log(10);
+
+// PETLJA (LOOP) PREDSTAVLJA NACIN DA SE NEKI KOD IZVRSI (uz odredjene uslove) UZ ODREDJENE USLOVE VISE PUTA
+
+// for petlja - Sintaksa:
+
+// for (statement1; statement2; statement3) {
+// kod za izvrsavanje u svakoj interaciji
+// }
+
+// statement1 predstavlja definisanje promenljive koja ce da predstavlja iterator.
+// statement2 predstavlja uslov (granicu) do koje vrednosti ce taj iterator da ide.
+// statement3 predstavlja povecanje (smanjenje) iteratora koje se desi na kraju svake iteracije.
+
+// for (let i = 1; i <= 10; i++) {
+//   console.log(i);
+// }
+
+// // Ispisati brojeve od 1 do 10, izuzev broja 2 i broja 5.
+// // 1. Nacin
+
+// for (let i = 1; i < 11; i++) {
+//   if (i !== 2 || i !== 5) {
+//     console.log(i);
+//   }
+// }
+
+// continue keyword (rezervisana rec) ako se nadje u nekoj interaciji , zapravo oznacava
+// da se ta interacija zanemari i da se nastavi dalje sa sledecom. (preskace je)
+
+// 2. Nacin
+// for (let i = 1; i < 11; i++) {
+//   if (i === 2 || i === 5) {
+//     continue;
+//   } else {
+//     console.log(i);
+//   }
+// }
+
+// break keyword (rezervisana rec) predstavlja da od tog trenutka for petlja prestaje sa radom.
+
+// ispisati sve brojeve od 1 do 10 ako je neki broj deljiv sa 7 neka se prekine petlja
+
+for (let broj = 1; broj < 11; broj++) {
+  if (broj % 7 === 0) {
+    break;
+  } else {
+    console.log(broj);
+  }
+}
+
+// Ispisati parne brojeve od 1 do 20 na dva nacina
+
+for (let broj = 2; broj < 21; broj++) {
+  if (broj % 2 === 0) {
+    console.log(broj);
+  }
+}
+
+// 2. nacin
+for (let broj = 2; broj <= 20; broj += 2) {
+  console.log(broj);
+}
