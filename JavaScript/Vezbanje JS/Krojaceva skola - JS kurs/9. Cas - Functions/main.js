40e092cd77c72b75f246c6cc3d4f36610a7665bd
amirz98 <amirzornic1@gmail.com>
2022-10-29 23:54:57 +0200

    functions uvod

---
+++ JavaScript/Vezbanje JS/Krojaceva skola - JS kurs/9. Cas - Functions/main.js
@@ -0,0 +1,18 @@
+// functions - (FUNKCIJE)
+// Funkcije su (imenovani) blok koda koji mozemo vise puta iskoristiti
+
+// function display(a) {
+//   console.log(a);
+// }
+
+// display("JavaScript");
+
+// display("Php");
+
+function sabiranje(broj1, broj2) {
+  console.log(broj1 + broj2);
+}
+
+sabiranje(10, 20);
+
+sabiranje(1400, 2000);
