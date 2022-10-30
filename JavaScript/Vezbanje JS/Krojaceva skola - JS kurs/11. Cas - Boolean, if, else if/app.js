404990c8bea9df5e813ba5886c0d5a79afe9eacf
amirz98 <amirzornic1@gmail.com>
2022-10-30 22:12:31 +0100

    Krojaceva skola 11 cas - boolean, if, else if ...

---
+++ JavaScript/Vezbanje JS/Krojaceva skola - JS kurs/11. Cas - Boolean, if, else if/app.js
@@ -0,0 +1,17 @@
+// var istina = true
+// var laz = false
+
+// var ar1 = [1, 2, 3];
+// var ar2 = [1, 2, 3];
+
+// console.log(ar1 === ar2);
+
+// Kompleksi data type-ovi (array, objects, function) nikada nisu jednaki jedan drugom cak iako imaju iste vrednosti unutar sebe.
+
+var password = Number(prompt("Pogodite broj"));
+if (password === 123) {
+  // drugi nacin staviti navodnike na "123" (string)
+  console.log("Pogodak");
+} else {
+  console.log("Promasaj");
+}
