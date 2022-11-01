23eccf3ca200181f5d25568237be026da128bf88
amirz98 <amirzornic1@gmail.com>
2022-11-01 19:07:56 +0100

    domaci

---
+++ JavaScript/Domaci zadaci/switch case/app.js
@@ -0,0 +1,31 @@
+// Domaci zadatak:
+// Preko switch naredbe:
+// console.log(new Date().getDay());
+// getDay() - daje vrednost 0-6
+// switch (new Date().getDay())...
+// Na osnovu vrednosti koju nam daje izraz iz switch naredbe, kroz case_ove i eventualno default ispisati poruku:
+// "Danas je 'radni dan'"
+// "Ugodno provedite vikend"
+
+switch (new Date().getDay()) {
+  case 1:
+    console.log("Danas je radni dan");
+    break;
+  case 2:
+    console.log("Danas je radni dan");
+    break;
+  case 3:
+    console.log("Danas je radni dan");
+    break;
+  case 4:
+    console.log("Danas je radni dan");
+    break;
+  case 5:
+    console.log("Danas je radni dan");
+    break;
+  case 6:
+    console.log("Ugodno provedite vikend");
+    break;
+  case 0:
+    console.log("Ugodno provedite vikend");
+}
