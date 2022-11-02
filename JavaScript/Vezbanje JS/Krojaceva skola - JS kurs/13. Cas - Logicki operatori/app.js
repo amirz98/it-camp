a34313a30e6dee03e229b34b2672b762e1a5fa26
amirz98 <amirzornic1@gmail.com>
2022-11-02 11:10:37 +0100

    krojaceva skoola

---
+++ JavaScript/Vezbanje JS/Krojaceva skola - JS kurs/13. Cas - Logicki operatori/app.js
@@ -0,0 +1,19 @@
+// var username = prompt("Unesite vas username");
+// var password = +prompt("Unesite vas pass");
+// var secretCode = "9999";
+// var code = prompt("Unesite tajni kod");
+
+// if ((username === "Amir" && password === 1998) || code === secretCode) {
+//   console.log("Tacno");
+// } else {
+//   console.log("Uneli ste pogresne podatke");
+// }
+
+var a = 15;
+a++;
+console.log(a);
+
+a === 16 ? console.log("Tacno") : console.log("Pogresno");
+
+// && (and) - da obe strane daju odgovor true (budu tacne)
+// || (or)  - da samo jedna strana bude true
