b6ca1037329c9bc46f474038d716d2e9bacb2777
amirz98 <amirzornic1@gmail.com>
2022-11-04 20:59:12 +0100

    domaci petlje

---
+++ JavaScript/Vezbanje JS/Krojaceva skola - JS kurs/14. Cas/app.js
@@ -0,0 +1,9 @@
+// var userAnswer = confirm("Da li je JS programski jezik ?");
+// console.log(userAnswer);
+
+var questions = [
+  {
+    text: "JS je programski jezik?",
+    correctAnswer: true,
+  },
+];
